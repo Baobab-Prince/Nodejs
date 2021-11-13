b14f5d94bb53d02a248d582651608e3890445037
Chaz Carothers <chaz.carothers@gmail.com>
2021-11-13 04:04:02 -0600

    updated packages

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Test');
+const chalk = require('chalk');
+
+console.log(
+  chalk.green('Congratulations! You are ') +
+    chalk.hex('#ff0000').underline('FIRED!') +
+    chalk.bgRed('GET OUT NOW!!!')
+);
